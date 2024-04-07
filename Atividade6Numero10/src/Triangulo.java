6d414bb8d478790727a37893c4a2a55cf6d0c1ef
ThiagoFPires <161969612+ThiagoFPires@users.noreply.github.com>
2024-04-07 18:34:31 -0300

    Add files via upload

---
+++ Atividade6Numero10/src/Triangulo.java
@@ -0,0 +1,40 @@
+class Triangulo {
+    private double lado1;
+    private double lado2;
+    private double lado3;
+
+    // utilizado construtor para inicializar os comprimentos dos lados do triângulo
+    public Triangulo(double lado1, double lado2, double lado3) {
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+
+    // função para calcular a área do triângulo
+    public double calcularArea() {
+        double s = (this.lado1 + this.lado2 + this.lado3) / 2.0;
+        double area = Math.sqrt(s * (s - this.lado1) * (s - this.lado2) * (s - this.lado3));
+        return area;
+    }
+
+    // função para calcular o perímetro do triângulo
+    public double calcularPerimetro() {
+        return this.lado1 + this.lado2 + this.lado3;
+    }
+
+    // função que determina o tipo do triângulo (Equilátero, Isósceles, Escaleno)
+    public String obterTipoTriangulo() {
+        if (this.lado1 == this.lado2 && this.lado1 == this.lado3) {
+            return "Equilátero";
+        } else if (this.lado1 != this.lado2 && this.lado1 != this.lado3 && this.lado2 != this.lado3) {
+            return "Escaleno";
+        } else {
+            return "Isósceles";
+        }
+    }
+
+    // função que verifica se o triângulo é equilátero
+    public String verificarEquilatero() {
+        return this.lado1 == this.lado2 && this.lado1 == this.lado3 ? "Sim" : "Não";
+    }
+}
